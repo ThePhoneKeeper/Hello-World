8eff1d3ee35cad6e27ca298912444613ec4d15a3
Rishab Nair <rishab9706@gmail.com>
2019-03-29 20:50:48 +0530

    Add button click support

--- app/src/main/java/com/rishab/helloworld/MainActivity.java
+++ app/src/main/java/com/rishab/helloworld/MainActivity.java
@@ -1,36 +1,25 @@
 package com.rishab.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private void goToSecondActivity() {
-
-        Intent intent = new Intent(this, SecondActivity.class);
-
-        startActivity(intent);
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
+        Button btn = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.onClickListener() {
-
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-
             public void onClick(View v) {
-
-                goToSecondActivity();
-
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
 
-        });
-    }
+    });
+   }
 }
